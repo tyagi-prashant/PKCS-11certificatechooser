52b628f5788cf4deab29842c60d3003f6fa9d83d
David Woodhouse <David.Woodhouse@intel.com>
2016-05-13 09:51:59 +0100

    Add GcrViewerWidget as preview

--- certificate_chooser.c
+++ certificate_chooser.c
@@ -7,6 +7,21 @@
 #include "certificate_renderer.c"
 
 static void
+on_update_preview(GtkWidget *widget, gpointer *data)
+{
+	GtkFileChooser *chooser = GTK_FILE_CHOOSER(widget);
+	char *filename = gtk_file_chooser_get_preview_filename(chooser);
+	if (!filename) {
+		gtk_file_chooser_set_preview_widget_active(chooser, FALSE);
+		return;
+	}
+	printf("Preview %s\n", filename);
+	gcr_viewer_widget_load_file(GCR_VIEWER_WIDGET(data), g_file_new_for_path(filename));
+	gtk_file_chooser_set_preview_widget_active(chooser, TRUE);
+	g_free(filename);
+}
+
+static void
 certificate_chooser (GtkWidget *widget,
              gpointer   data)
 {
@@ -16,7 +31,7 @@ certificate_chooser (GtkWidget *widget,
   GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
   gint res;
 
-  dialog = gtk_file_chooser_dialog_new ("Open File",
+  dialog = gtk_file_chooser_dialog_new ("Select Certificate",
                                       data,
                                       action,
                                       ("Cancel"),
@@ -36,10 +51,16 @@ certificate_chooser (GtkWidget *widget,
   gtk_file_filter_add_pattern(filefilter,"*.p12");
   GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
   gtk_file_chooser_add_filter(chooser, filefilter); 
- 
+
+  GcrViewerWidget *viewer = gcr_viewer_widget_new();
+  g_signal_connect(chooser, "update-preview", G_CALLBACK (on_update_preview), viewer);
+  gtk_file_chooser_set_preview_widget(chooser, GTK_WIDGET(viewer));
+
+  
   res = gtk_dialog_run (GTK_DIALOG (dialog));
   if (res == GTK_RESPONSE_ACCEPT) {
     char *uri;
+    
     uri = gtk_file_chooser_get_filename (chooser);
     loaded_certificate(uri);
     g_print("%s\n", uri);
